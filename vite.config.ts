832e72e0d43dda224dec177129d8eb4035b3e5c4
Naoki Ainoya <ainonic@gmail.com>
2024-08-20 21:33:52 +0900

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 export default defineConfig({
   base: process.env.GITHUB_PAGES ? "/llm-output-format-gen/" : "/",
@@ -12,6 +14,15 @@ export default defineConfig({
         v3_throwAbortReason: true,
       },
       ssr: false,
+      basename: process.env.GITHUB_PAGES ? "/llm-output-format-gen/" : "/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
+      },
     }),
     tsconfigPaths(),
   ],
